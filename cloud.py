d6051d1eb42d2ed759701b3bf9cdb0d2d9000af8
Wayho <34697764+Wayho@users.noreply.github.com>
2018-04-19 12:59:31 +0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -6,6 +6,7 @@ import subprocess
 import select
 import requests
 import time
+import random
 import os   # 在云引擎 Python 环境中使用自定义的环境变量,WORK_ID=1
 import psutil
 
@@ -231,7 +232,7 @@ def Mine_xmr_stak_Monero():
 	XMRSTAK_RUNNING = True
 	print 'Mine_xmr_stak_Monero:Once'
 	OutputShell('chmod +x xmrstak40')
-	time.sleep(1)
+	time.sleep(random.randint(30, 60))
 	str_cmd = './xmrstak40'
 	return MineShell(str_cmd)
 
@@ -244,7 +245,7 @@ def Mine_xmr_stak_Monero40s():
 	XMRSTAK_RUNNING = True
 	print 'Mine_xmr_stak_Monero:Once'
 	OutputShell('chmod +x xmrstak33s16')
-	time.sleep(1)
+	time.sleep(random.randint(30, 60))
 	str_cmd = STR_CMD_MINE + 'xmrstak33s16'
 	return MineShell(str_cmd)
 
@@ -257,7 +258,7 @@ def Mine_xmr_stak_Monero36s():
 	XMRSTAK_RUNNING = True
 	print 'Mine_xmr_stak_Monero:Once'
 	OutputShell('chmod +x xmrstak36s16')
-	time.sleep(1)
+	time.sleep(random.randint(30, 60))
 	str_cmd = './xmrstak36s16'
 	return MineShell(str_cmd)
 
